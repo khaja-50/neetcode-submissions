6f85b2ecb63a0ed2ea3f8445a6c97ee344d27043
Khaja Rizwan Ali <khawajarizwanali123@gmail.com>
2026-08-16 01:38:47 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int n=nums.size();
+        vector<int> ans;
+        for(int num:nums){
+            mp[num]++;
+        }
+        for(auto &p:mp){
+            if(p.second>n/3) ans.push_back(p.first);
+        }
+        return ans;
+    }
+};
